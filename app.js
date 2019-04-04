05bea1793e2282f6f81e5f20687f22bc19be2575
June Hyok Cho <jcho0627@gmail.com>
2019-04-04 06:48:15 +0200

    post request friends list update complete

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let friends = ["Tony", "Miranda", "Justin", "Pierre", "Lily"];
 
 app.get("/", function (req, res) {
   res.render("home");
 })
 
 app.get("/friends", function (req, res) {
-  let friends = ["Tony", "Miranda", "Justin", "Pierre", "Lily"];
   res.render("friends", { friends: friends });
 })
 
 app.post("/addfriend", function (req, res) {
-  res.send("You have reached the post route!");
+  let newFriend = req.body.newFriend;
+  friends.push(newFriend);
+  res.redirect("/friends");
 });
 
 app.listen(3000, () => "Server is listening on port 3000!");
